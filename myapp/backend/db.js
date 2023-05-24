e93a73b75679e04dce7f6d1060bc717a1ad0ca79
yash <slayycasm997@gmail.com>
2023-05-24 17:03:38 +0530

    fixed the error in first post request and now its perfectly working fine

--- myapp/backend/db.js
+++ myapp/backend/db.js
@@ -1,37 +1,16 @@
  //connection to database
  const mongoose = require('mongoose')
- const myFunction =()=>{ mongoose.connect('mongodb+srv://yashd:yashiitian@nodeexpressproject.qp0arwg.mongodb.net/gofoodmern?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+ const myFunction =async()=>{ await mongoose.connect('mongodb+srv://yashd:yashiitian@nodeexpressproject.qp0arwg.mongodb.net/gofoodmern?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
  .then(async()=>{
      console.log("connected...")
-     const userSchema = new mongoose.Schema({
-         name: {
-             type:String,
-             required:true
-         },
-         location: {
-             type:String,
-             required:true
-         },
-         email: {
-             type:String,
-             required:true
-         },
-         password: {
-             type:String,
-             required:true
-         },
-       });
-     const users = mongoose.model('users', userSchema);
-     try {
-         const data = await users.find({});
-         console.log(data)
-     } catch (error) {
-         console.log(error)
-     }
- 
- 
- 
-     
+
+    //  const users = mongoose.model('users', userSchema);
+    //  try {
+    //      const data = await users.find({});
+    //      console.log(data)
+    //  } catch (error) {
+    //      console.log(error)
+    //  }    
  })
  .catch((error)=>{
      console.error("error has been occured...",error)
